727b9337d46a3ad91142927b85f95693b697b91b
Henry Burgess <henryjburg@gmail.com>
2024-02-20 15:51:46 -0600

    Revert "RDK-20 Apply updates"
    This reverts commit d03f731db3c5cccf4b5f912d4746bcf1dfad43df.

--- Assets/Scripts/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager.cs
@@ -7,32 +7,16 @@ using System.IO;
 // add the UXF namespace
 using UXF;
 
-public class ExperimentManager : MonoBehaviour
+public class SessionGenerator : MonoBehaviour
 {
-    /// <summary>
-    /// Generate the experiment timeline
-    /// </summary>
-    /// <param name="session"></param>
     public void GenerateExperiment(Session session)
     {
         int numMainTrials = 1;
         Block mainBlock = session.CreateBlock(numMainTrials);
     }
 
-    /// <summary>
-    /// Commence the experiment
-    /// </summary>
-    /// <param name="session"></param>
     public void BeginExperiment(Session session)
     {
         session.BeginNextTrial();
-    }
-
-    /// <summary>
-    /// Close the application, perform any cleanup if required
-    /// </summary>
-    public void QuitApplication()
-    {
-        Application.Quit();
     }
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// add the UXF namespace
-using UXF;
-
-public class InputHandler : MonoBehaviour
+public class NewMonoBehaviour : MonoBehaviour
 {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        // End the current trial when the trigger button is pressed
-        if (OVRInput.Get(OVRInput.Button.One) == true)
-        {
-            Session.instance.EndCurrentTrial();
-        }
+
     }
 }
