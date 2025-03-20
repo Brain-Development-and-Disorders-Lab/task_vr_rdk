65df3a2e5ef825eb1a7c2fd20b94b6ef6dca43e6
Henry Burgess <henryjburg@gmail.com>
2025-03-19 21:55:20 -0500

    RDK-79 Add `setup` and `validation` stages

--- Assets/Scripts/SetupManager.cs
+++ Assets/Scripts/SetupManager.cs
@@ -9,6 +9,9 @@ using Utilities;
 
 /// <summary>
 /// Manager for headset setup operations. Currently handles setup, operation, and eye-tracking calibration.
+/// Calibration has two phases:
+/// 1. Setup - Move the fixation object around the screen to collect data
+/// 2. Validation - Move the fixation object around the screen to validate the data, with a smaller threshold
 /// </summary>
 public class SetupManager : MonoBehaviour
 {
@@ -27,14 +30,16 @@ public class SetupManager : MonoBehaviour
     private EyePositionTracker _rightEyeTracker;
 
     // Flags for state management
-    private bool _isEyeTrackingSetupActive = false; // 'true' when running calibration operations
     private bool _fixationCanProceed = false; // 'true' when the fixation object can proceed to the next position
-    private bool _isEyeTrackingSetupComplete = false; // 'true' once operations complete
+    private bool _isEyeTrackingCalibrationActive = false; // 'true' when running calibration operations
+    private bool _isEyeTrackingCalibrationSetup = false; // 'true' once the eye tracking calibration is validated
+    private bool _isEyeTrackingCalibrationComplete = false; // 'true' once operations complete
 
     // Set of points to be displayed for fixation and the "path" of the fixation object used
     // for eye-tracking setup
     private readonly float _fixationRadius = 2.4f;
-    private readonly float _fixationThreshold = 0.25f;
+    private readonly float _fixationSetupThreshold = 0.50f;
+    private readonly float _fixationValidationThreshold = 0.25f;
     private GameObject _fixationObject; // Object moved around the screen
     private Vector2 _fixationObjectPosition; // The active unit vector
     private int _fixationObjectPositionIndex = 0;
@@ -51,7 +56,16 @@ public class SetupManager : MonoBehaviour
     private Action _setupCallback; // Optional callback function executed after calibration complete
 
     // Data storage
-    private readonly Dictionary<string, List<GazeVector>> _gazeData = new() {
+    private readonly Dictionary<string, List<GazeVector>> _setupData = new() {
+        {"c_start", new List<GazeVector>() },
+        {"q_1", new List<GazeVector>() },
+        {"q_2", new List<GazeVector>() },
+        {"q_3", new List<GazeVector>() },
+        {"q_4", new List<GazeVector>() },
+        {"c_end", new List<GazeVector>() },
+    };
+
+    private readonly Dictionary<string, List<GazeVector>> _validationData = new() {
         {"c_start", new List<GazeVector>() },
         {"q_1", new List<GazeVector>() },
         {"q_2", new List<GazeVector>() },
@@ -92,18 +106,36 @@ public class SetupManager : MonoBehaviour
     /// <param name="callback">Optional callback function to execute at calibration completion</param>
     public void RunSetup(Action callback = null)
     {
-        _isEyeTrackingSetupActive = true;
-        _fixationObject.SetActive(_isEyeTrackingSetupActive);
+        _isEyeTrackingCalibrationActive = true;
+        _fixationObject.SetActive(_isEyeTrackingCalibrationActive);
 
         // Optional callback function
         _setupCallback = callback;
     }
 
-    private void EndSetup()
+    private void EndCalibrationStage()
     {
-        _isEyeTrackingSetupActive = false;
-        _isEyeTrackingSetupComplete = true;
-        _fixationObject.SetActive(_isEyeTrackingSetupActive);
+        Debug.Log("Ending calibration stage...");
+        if (!_isEyeTrackingCalibrationSetup)
+        {
+            // Completed the setup stage, begin the validation stage
+            _isEyeTrackingCalibrationSetup = true;
+            _isEyeTrackingCalibrationActive = true;
+            Debug.Log("Completed the setup stage, beginning the validation stage...");
+        }
+        else
+        {
+            // Completed the validation stage, end the calibration
+            Debug.Log("Completed the validation stage, ending the calibration...");
+            EndCalibration();
+        }
+    }
+
+    private void EndCalibration()
+    {
+        _isEyeTrackingCalibrationActive = false;
+        _isEyeTrackingCalibrationComplete = true;
+        _fixationObject.SetActive(_isEyeTrackingCalibrationActive);
 
         // Run calculation of gaze offset values
         CalculateOffsetValues();
@@ -115,21 +147,21 @@ public class SetupManager : MonoBehaviour
         _setupCallback?.Invoke();
     }
 
-    public bool GetCalibrationComplete() => _isEyeTrackingSetupComplete;
+    public bool GetCalibrationComplete() => _isEyeTrackingCalibrationComplete;
 
-    public bool GetCalibrationActive() => _isEyeTrackingSetupActive;
+    public bool GetCalibrationActive() => _isEyeTrackingCalibrationActive;
 
     public void SetViewCalibrationVisibility(bool state) => _viewCalibrationPrefabInstance.SetActive(state);
 
     private void CalculateOffsetValues()
     {
         // Function to examine each point and calculate average vector difference from each point
-        foreach (string _unitVectorDirection in _gazeData.Keys)
+        foreach (string _unitVectorDirection in _setupData.Keys)
         {
             var L_vectorSum = Vector2.zero;
             var R_vectorSum = Vector2.zero;
 
-            foreach (var VectorPair in _gazeData[_unitVectorDirection])
+            foreach (var VectorPair in _setupData[_unitVectorDirection])
             {
                 // Get the sum of the gaze vector and the actual position of the dot for each eye
                 var L_result = new Vector2(VectorPair.GetLeft().x, VectorPair.GetLeft().y) + (_fixationObjectPath[_unitVectorDirection] * _fixationRadius);
@@ -139,8 +171,8 @@ public class SetupManager : MonoBehaviour
                 R_vectorSum += new Vector2(VectorPair.GetRight().x, VectorPair.GetRight().y) + (_fixationObjectPath[_unitVectorDirection] * _fixationRadius);
             }
 
-            L_vectorSum /= _gazeData[_unitVectorDirection].Count;
-            R_vectorSum /= _gazeData[_unitVectorDirection].Count;
+            L_vectorSum /= _setupData[_unitVectorDirection].Count;
+            R_vectorSum /= _setupData[_unitVectorDirection].Count;
             _directionalOffsets.Add(_unitVectorDirection, new GazeVector(L_vectorSum, R_vectorSum));
         }
 
@@ -148,14 +180,14 @@ public class SetupManager : MonoBehaviour
         var L_averageOffsetCorrection = Vector2.zero;
         var R_averageOffsetCorrection = Vector2.zero;
 
-        foreach (string _unitVectorDirection in _gazeData.Keys)
+        foreach (string _unitVectorDirection in _setupData.Keys)
         {
             L_averageOffsetCorrection += _directionalOffsets[_unitVectorDirection].GetLeft();
             R_averageOffsetCorrection += _directionalOffsets[_unitVectorDirection].GetRight();
         }
 
-        L_averageOffsetCorrection /= _gazeData.Keys.Count;
-        R_averageOffsetCorrection /= _gazeData.Keys.Count;
+        L_averageOffsetCorrection /= _setupData.Keys.Count;
+        R_averageOffsetCorrection /= _setupData.Keys.Count;
     }
 
     public Dictionary<string, GazeVector> Get_directionalOffsets() => _directionalOffsets;
@@ -175,13 +207,48 @@ public class SetupManager : MonoBehaviour
     /// <param name="l_p">Left eye tracking data</param>
     /// <param name="r_p">Right eye tracking data</param>
     /// <returns>True if the eye tracking data is within the fixation threshold, false otherwise</returns>
-    private bool IsFixated(Vector3 l_p, Vector3 r_p) => Vector2.Distance(l_p, _fixationObject.transform.position) < _fixationThreshold && Vector2.Distance(r_p, _fixationObject.transform.position) < _fixationThreshold;
+    private bool IsFixated(Vector3 l_p, Vector3 r_p)
+    {
+        // Determine which threshold to use based on the current state
+        if (!_isEyeTrackingCalibrationSetup)
+        {
+            // Setup stage, use the setup threshold
+            return Vector2.Distance(l_p, _fixationObject.transform.position) < _fixationSetupThreshold && Vector2.Distance(r_p, _fixationObject.transform.position) < _fixationSetupThreshold;
+        }
+        else
+        {
+            // Validation stage, use the validation threshold
+            return Vector2.Distance(l_p, _fixationObject.transform.position) < _fixationValidationThreshold && Vector2.Distance(r_p, _fixationObject.transform.position) < _fixationValidationThreshold;
+        }
+    }
+
+    private void RunGazeCapture()
+    {
+        // Capture eye tracking data and store alongside location
+        var l_p = _leftEyeTracker.GetGazeEstimate();
+        var r_p = _rightEyeTracker.GetGazeEstimate();
+
+        // Determine which data dictionary to use based on the current state
+        var gazeData = _isEyeTrackingCalibrationSetup ? _validationData : _setupData;
+
+        // Test fixation and add to the appropriate data dictionary
+        if (IsFixated(l_p, r_p))
+        {
+            gazeData[_fixationObjectPath.Keys.ToList()[_fixationObjectPositionIndex]].Add(new(l_p, r_p));
+        }
+
+        // If the number of fixations is greater than or equal to 50, proceed to the next position
+        if (gazeData[_fixationObjectPath.Keys.ToList()[_fixationObjectPositionIndex]].Count >= 50)
+        {
+            _fixationObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+            _fixationCanProceed = true;
+        }
+    }
 
     private void Update()
     {
-        if (_isEyeTrackingSetupActive)
+        if (_isEyeTrackingCalibrationActive)
         {
-
             if (_fixationCanProceed)
             {
                 _updateTimer += Time.deltaTime;
@@ -192,7 +259,7 @@ public class SetupManager : MonoBehaviour
                     if (_fixationObjectPositionIndex > _fixationObjectPath.Count - 1)
                     {
                         _fixationObjectPositionIndex = 0;
-                        EndSetup();
+                        EndCalibrationStage();
                     }
                     _fixationObjectPosition = _fixationObjectPath[_fixationObjectPath.Keys.ToList()[_fixationObjectPositionIndex]];
                     _fixationObject.transform.localPosition = new Vector3(_fixationObjectPosition.x * _fixationRadius, _fixationObjectPosition.y * _fixationRadius, 0.0f);
@@ -205,21 +272,7 @@ public class SetupManager : MonoBehaviour
             }
             else
             {
-                // Capture eye tracking data and store alongside location
-                var l_p = _leftEyeTracker.GetGazeEstimate();
-                var r_p = _rightEyeTracker.GetGazeEstimate();
-
-                if (IsFixated(l_p, r_p))
-                {
-                    _gazeData[_fixationObjectPath.Keys.ToList()[_fixationObjectPositionIndex]].Add(new(l_p, r_p));
-                }
-
-                // If the number of fixations is greater than or equal to 50, proceed to the next position
-                if (_gazeData[_fixationObjectPath.Keys.ToList()[_fixationObjectPositionIndex]].Count >= 50)
-                {
-                    _fixationObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
-                    _fixationCanProceed = true;
-                }
+                RunGazeCapture();
             }
         }
     }
